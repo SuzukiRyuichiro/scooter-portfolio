40a9cc1fa115ef51aae52152156121532becb6c8
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-06-13 14:45:40 +0900

    more japanese

--- source/assets/javascripts/src/controllers/translate_controller.js
+++ source/assets/javascripts/src/controllers/translate_controller.js
@@ -2,7 +2,7 @@ import { Controller } from 'stimulus'
 
 export default class extends Controller {
 
-  static targets = ['name', 'subtitle']
+  static targets = ['name', 'subtitle', 'Info', 'Projects', 'Contact']
 
   connect() {
 
@@ -11,5 +11,8 @@ export default class extends Controller {
   test() {
     this.nameTarget.innerText = "鈴木龍一郎";
     this.subtitleTarget.innerText = "よろしくお願いします！";
+    this.InfoTarget.innerText = "自己紹介";
+    this.ProjectsTarget.innerText = "プロジェクト";
+    this.ContactTarget.innerText = "連絡先";
   }
 }
