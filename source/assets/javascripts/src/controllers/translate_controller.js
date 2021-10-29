b4cb2b97c05e45e233ceeb9e876e040ecf1a19f3
Scooter <dragon.aka.scooter@gmail.com>
2021-10-29 15:07:11 +0900

    made some update

--- source/assets/javascripts/src/controllers/translate_controller.js
+++ source/assets/javascripts/src/controllers/translate_controller.js
@@ -33,7 +33,7 @@ export default class extends Controller {
     this.ContactTarget.classList.add("japanese");
     this.SocialsTarget.innerText = "ソーシャルメディア";
     this.heyTarget.innerText = "こんにちは！";
-    this.aboutmeTarget.innerText =　"フルスタックエンジニアの鈴木龍一郎です。文系の学部に通っていますが、基本的にずっとプログラミングの勉強をしています。大学3年のときに、Le Wagon Tokyoで9週間のフルタイム・コーディング・ブートキャンプに参加し、卒業後はそこでTAもやっています。日本語と英語のバイリンガルです🇯🇵🇬🇧";
+    this.aboutmeTarget.innerText =　"フルスタックエンジニアの鈴木龍一郎です。文系の学部に通っていますが、基本的にずっとプログラミングの勉強をしています。大学3年のときに、Le Wagon Tokyoで9週間のフルタイム・コーディング・ブートキャンプに参加し、卒業後はそこでTAもやっています。現在は、TAに加えて、2つの会社でフルスタックエンジニアとしてフリーランスをしています。使用言語は、Ruby, PHP, JS, フレームワークはRails, Vue, あとは社内フレームワークを使って開発しています。日本語と英語のバイリンガルです🇯🇵🇬🇧";
     this.InfoTarget.classList.add("japanese");
   }
 
@@ -50,7 +50,7 @@ export default class extends Controller {
     this.ContactTarget.innerText = "Contact";
     this.SocialsTarget.innerText = "Socials";
     this.heyTarget.innerText = "hey👋 I'm Scooter 🛵";
-    this.aboutmeTarget.innerText =　"I am a full-stack web developer and very passionate about technology and computer science. I participated in 9 week full-time coding bootcamp at Le Wagon Tokyo and graduated in March 2021. Since then, I am learning new concepts everyday to become a better developer. I am a Japanese / English bilingual 🇯🇵🇬🇧";
+    this.aboutmeTarget.innerText =　"I am a full-stack web developer and very passionate about technology and computer science. I participated in 9 week full-time coding bootcamp at Le Wagon Tokyo and graduated in March 2021. Since then, I am learning new concepts everyday to become a better developer. I am currently freelancing in two different companies as a fullstack developer using Rails, Vue, PHP, and many other tools. I am a Japanese / English bilingual 🇯🇵🇬🇧";
   }
 
   toggle() {
